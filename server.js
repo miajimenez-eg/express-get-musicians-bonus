af3b49ebd2373dd2ce255cf1bf00793d8a2afc01
Mia Jimenez <miajimenez@expediagroup.com>
2023-02-22 17:44:51 +0000

    add get request for specific band including its musicians

--- server.js
+++ server.js
@@ -6,6 +6,28 @@ const {Band} = require("./Band")
 const {Sequelize} = require("./db");
 const sequelize = require("sequelize");
 
+app.use(express.json())
+app.get('/bands', async (req, res) => {
+    const allBands = await Band.findAll({
+        include: [{
+            model: Musician
+        }
+        ]
+    });
+
+})
+
+app.use(express.json())
+app.get('/bands/:id', async (req, res) => {
+    let id = req.params.id;
+    const getBand = await Band.findByPk(id, {
+        include: [{
+            model: Musician, where: { bandId: id }
+        }]
+    })
+    res.json(getBand);
+})
+
 app.listen(port, () => {
     sequelize.sync()
     console.log("App listening on port " + port)
